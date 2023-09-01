c4021d3f9a08c976eed124c4f86aeefb481e6b4a
Negin Mashhadi <44472167+neginmashhadi@users.noreply.github.com>
2023-08-31 19:13:24 -0700

    Add files via upload

---
+++ CS1B/Labs/Lab4/headerFunction.cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+ * AURHOR			:	Negin Mashhadi	& Gianna Ta
+ * STUDENT ID		:	1084104			& 1050660
+ * Lab#4			:	Introduction to Arrays
+ * CLASS			:	CS1B
+ * SECTION			:	MW - 6:30pm - 9:50pm
+ * DUE DATE			:	2/14/2018
+ * ****************************************************************************/
+#include "lab4HeaderFile.h"
+/******************************************************************************
+ * FUNCTION printHeader
+ * -----------------------------------------------------------------------------
+ * 	This function receives an assignment name, type and number then outputs the
+ * 		appropriate class heading.
+ * ==> returns nothing - this function output the class heading.
+ * -----------------------------------------------------------------------------
+ * PRE-CONDITIONS
+ *			the following need a defined value pass in
+ *			output  : The output file
+ *			asName:	Assignment Name
+ *			asType: Assignment Type
+ *			asNum : Assignment Number
+ *
+ * POST-CONDITIONS
+ * 		==>	Returns nothing - this function output the class heading.
+ ******************************************************************************/
+
+void PrintHeader (ostream  &output,	// IN/OUT - output file
+				  string	asName,	// IN 	  - assignment Name - used for output
+				  char		asType, // IN 	  - assignment Type
+				   	   	   	   	   	//	      - (LAB or ASSIGN) - used for output
+				  int		asNum)	// IN     - assignment Name - used for output
+
+{
+	output << left;
+	output << "*************************************************************\n";
+	output << "* PROGRAMMED BY : Negin Mashhadi & Gianna Ta\n";
+	output << "* "	  << setw(14) << "STUDENT ID" << ": 1084104 & 1050660\n";
+	output << "* "    << setw(14) << "CLASS"      << ": CS1B - MW - 6:30pm\n";
+	output << "* ";
+
+	//PROCESSING - This will adjust setws and format appropriately based on if
+	//				this is a lab 'L' or assignment
+
+	if (toupper(asType) == 'L')
+	{
+		output << "LAB # " << setw(8);
+	}
+	else
+	{
+		output << "ASSIGNMENT #" << setw(2);
+	}
+
+	output << asNum << ": " << asName << endl;
+	output << "*************************************************************\n\n";
+	output << right<< endl;
+
+}
